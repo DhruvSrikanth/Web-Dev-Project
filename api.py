4fb5563bf3003c0d77adda1abc5551e797e5af81
redikurti <rkurti@uchicago.edu>
2022-06-01 22:31:05 -0500

    made it work

--- api.py
+++ api.py
@@ -9,11 +9,9 @@ app = Flask(__name__)
 # conn = sqlite3.connect('')
 # cur = conn.cursor()
 
-@app.route('/login', methods=['GET'])
+@app.route('/', methods=['GET'])
 def login():
-    print("Login Page")
-    return render_template('../templates/login/login.html')   
-
+    return render_template('login/login.html')
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
